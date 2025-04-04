78aee352c15e3bf53c643c604c9b8303219ff126
Zid Visda <rashidvisda@gmail.com>
2025-04-04 17:03:24 +0800

    Added search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const SearchBar = () => {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <label className="relative w-full">
+        <input
+          type="search"
+          required
+          placeholder="Search"
+          className="w-full p-2 dark:text-white pl-10 border focus:border-violet-400 border-grayCustom rounded-xl  focus:outline-none focus:border-2 dark:focus:border"
+        />
+        <svg
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 w-[1em] h-[1em] opacity-100 stroke-black dark:stroke-white"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="2.5"
+            fill="none"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </g>
+        </svg>
+      </label>
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/pages/DictionaryPage.jsx
+++ src/pages/DictionaryPage.jsx
@@ -4,9 +4,11 @@ import SearchBar from "../components/SearchBar";
 import WordCard from "../components/WordCard";
 const DictionaryPage = () => {
   return (
-    <div className="container max-w-[1024px] mx-auto p-4 md:p-6 lg:p-10">
+    <div className="container  max-w-[1024px] mx-auto p-4 md:p-6 lg:p-10">
       <Navbar />
-      <SearchBar />
+      <div className="mt-12">
+        <SearchBar />
+      </div>
       <WordCard />
     </div>
   );
